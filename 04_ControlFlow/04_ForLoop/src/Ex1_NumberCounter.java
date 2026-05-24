15e9f90745f2bf634a8297d6f5a860e1251512c5
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-24 08:01:06 +0800

    04_ControlFlow: For Loop - Number Counting.

---
+++ 04_ControlFlow/04_ForLoop/src/Ex1_NumberCounter.java
@@ -0,0 +1,11 @@
+public class Ex1_NumberCounter {
+    public static void main(String[] args){
+
+        System.out.println("Project Name: Number Counter");
+        System.out.println();
+        for(int i = 0; i <= 20; i++){
+            System.out.println("Counting: "+ i);
+        }
+        System.out.println("End Of Program...");
+    }
+}
