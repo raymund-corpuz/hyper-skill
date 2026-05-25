a48e1ce5eb8b51ead4d81c3928b05e70eac418a1
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-25 08:04:29 +0800

    04_ControlFlow: While Loop - Alphabet.

---
+++ 04_ControlFlow/05_WhileLoop/src/Ex4_Alphabet.java
@@ -0,0 +1,13 @@
+public class Ex4_Alphabet {
+    public static void main(String[] args) {
+
+        char letter = 'A';
+        System.out.println("Project Name: Aplhabet");
+        while (letter <= 'Z') {
+            System.out.print(letter + ", ");
+            letter++;
+        }
+        System.out.println();
+        System.out.println("End Of Program...");
+    }
+}
