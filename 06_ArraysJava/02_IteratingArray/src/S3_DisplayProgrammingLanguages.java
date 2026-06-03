43656f3efac3d9bac59b1d80bd2167dc524c9d30
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-06-03 09:20:08 +0800

    06_ArraysJava: Iterating Over an Array - Sample3: Displaying Programming Languages.

---
+++ 06_ArraysJava/02_IteratingArray/src/S3_DisplayProgrammingLanguages.java
@@ -0,0 +1,14 @@
+public class S3_DisplayProgrammingLanguages {
+    public static void main(String[] args) {
+        String[] programmingLanguages = {"Java", "C#", "C++", "Swift", "Python"};
+
+        System.out.println("Sample 3: Display Programming Languages");
+        System.out.println();
+
+        for (String program : programmingLanguages) {
+            System.out.println(program);
+        }
+        System.out.println();
+        System.out.println("End Of Program...");
+    }
+}
