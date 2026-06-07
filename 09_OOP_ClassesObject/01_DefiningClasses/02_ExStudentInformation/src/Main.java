9c298db9beeaf4d250a611358228e4ad91f1ff3c
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-06-07 10:02:03 +0800

    09_OOP_ClassesObject: Defining Classes - Student Information System.

---
+++ 09_OOP_ClassesObject/01_DefiningClasses/02_ExStudentInformation/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student raemun = new Student();
+
+        raemun.name = "Raemun";
+        raemun.age = 31;
+        raemun.course = "Computer Science";
+
+
+        System.out.println("Project Name: Student Information System");
+        System.out.println();
+        System.out.println("==== STUDENT INFORMATION ====");
+        System.out.println("Student Name: " + raemun.name);
+        System.out.println("Age: " + raemun.age);
+        System.out.println("Course: " + raemun.course);
+        System.out.println();
+        System.out.println("End Of Program....");
+
+    }
+}
