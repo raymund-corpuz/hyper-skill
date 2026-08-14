36193faded5165252218a7c2e84425291aa83b91
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-08-14 10:19:30 +0800

    11_ErrorHandling: Bank Account Management System - CRUD OPERATION.

--- 11_ErrorHandling/src/com/bank/app/Main.java
+++ 11_ErrorHandling/src/com/bank/app/Main.java
@@ -7,6 +7,7 @@ import com.bank.account.SavingsAccount;
 import com.bank.people.Customer;
 import com.bank.record.TransactionRecord;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -33,9 +34,10 @@ public class Main {
         accounts.add(new BusinessAccount("ACC-004", customers.get(3), 100_000, true));
         accounts.add(new CheckingAccount("ACC-005", customers.get(4), 8_000, true));
 
-        displayCustomers(customers);
-        addCustomer(customers, scanner);
-        displayCustomers(customers);
+        boolean running = true;
+        while (running) {
+            displayMenu(scanner, customers, running);
+        }
 
         scanner.close();
     }
@@ -121,9 +123,99 @@ public class Main {
         customers.remove(select);
         System.out.println();
     }
+
     //open Account
+    public static void openAccount(ArrayList<BankAccount> accounts, Customer customer, Scanner scanner) {
+        System.out.println("==== OPEN ACCOUNT ====");
+        System.out.println();
+        System.out.println("Enter Account Number: ");
+        String accountNumber = scanner.nextLine();
+        System.out.println("Enter Balance: ");
+        double balance = scanner.nextDouble();
+        scanner.nextLine();
+        if (balance <= 0) {
+            System.out.println("Balance must be greater than 1000");
+            return;
+        }
+        System.out.println("Enter Activate: ");
+        String activate = scanner.nextLine();
+        boolean isActivate = false;
+        if (activate.equalsIgnoreCase("Activate")) {
+            isActivate = true;
+            System.out.println("Successfully Activated the Account. ✅");
+        } else {
+            System.out.println("Unable to Activate the Account. ❌");
+            return;
+        }
+        System.out.println("Bank Account: Business / Checking / Savings");
+        String type = scanner.nextLine();
+
+        switch (type) {
+            case "Business":
+                accounts.add(new BusinessAccount(accountNumber, customer, balance, isActivate));
+                System.out.println("Successfully Open a Business Account.✅");
+                break;
+            case "Savings":
+                accounts.add(new SavingsAccount(accountNumber, customer, balance, isActivate));
+                System.out.println("Successfully Open a Savings Account.✅");
+                break;
+            case "Checking":
+                accounts.add(new CheckingAccount(accountNumber, customer, balance, isActivate));
+                System.out.println("Successfully Open a Checking Account.✅");
+                break;
+            default:
+                System.out.println("Invalid Type.❌");
+                break;
+        }
+
+    }
 
     //update Account
+    public static void updateAccount(ArrayList<BankAccount> accounts, Customer customer, Scanner scanner) {
+        System.out.println("==== UPDATE BANK ACCOUNT ====");
+        System.out.println();
+        int chosenAccount = selectAccount(scanner, accounts);
+        System.out.println("Enter Account Number: ");
+        String accountNumber = scanner.nextLine();
+        System.out.println("Enter Balance: ");
+        double balance = scanner.nextDouble();
+        if (balance <= 1000) {
+            System.out.println("Balance must be greater than 1000");
+            return;
+        }
+        System.out.println("Enter Activate: ");
+        String activate = scanner.nextLine();
+        boolean isActivate = false;
+
+        if (activate.equalsIgnoreCase("Activate")) {
+            isActivate = true;
+            System.out.println("Successfully Activated the Account. ✅");
+        } else {
+            System.out.println("Unable to Activate the Account. ❌");
+            return;
+        }
+        System.out.println("Bank Account: Business / Checking / Savings");
+        String type = scanner.nextLine();
+
+        switch (type) {
+            case "Business":
+                accounts.set(chosenAccount, new BusinessAccount(accountNumber, customer, balance, isActivate));
+                System.out.println("Successfully Open a Business Account.✅");
+                break;
+            case "Savings":
+                accounts.set(chosenAccount, new SavingsAccount(accountNumber, customer, balance, isActivate));
+                System.out.println("Successfully Open a Savings Account.✅");
+                break;
+            case "Checking":
+                accounts.set(chosenAccount, new CheckingAccount(accountNumber, customer, balance, isActivate));
+                System.out.println("Successfully Open a Checking Account.✅");
+                break;
+            default:
+                System.out.println("Invalid Type.❌");
+                break;
+        }
+    }
+
     //close Account
     //select Customer
     public static int selectCustomer(Scanner scanner, ArrayList<Customer> customers) {
@@ -157,9 +249,81 @@ public class Main {
         }
         return select;
     }
+
     //deposit
     //withdraw
     //view transaction history
     //display Main Menu
-    //process Menu
+    public static void displayMenu(Scanner scanner, ArrayList<Customer> customers, boolean running) {
+        System.out.println("==== BANKING ACCOUNT SYSTEM ====");
+        System.out.println();
+        System.out.println("1 - Customer Management ");
+        System.out.println();
+        System.out.println("2 - Account Management");
+        System.out.println();
+        System.out.println("3 - Transaction Management");
+        System.out.println();
+        System.out.println("4 - Exit");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                displayCustomerMenu(scanner, customers);
+                break;
+            case 2:
+                System.out.println("Account Menu");
+                break;
+            case 3:
+                System.out.println("Transaction Menu");
+                break;
+            case 4:
+                System.out.println("Exiting Program...");
+                running = false;
+                break;
+            default:
+                System.out.println("Invalid Choice. ❌");
+                running = false;
+                break;
+        }
+    }
+
+    //Customer Menu
+    public static void displayCustomerMenu(Scanner scanner, ArrayList<Customer> customers) {
+        System.out.println("==== CUSTOMER MENU ====");
+        System.out.println();
+        System.out.println("1 - Add Customer");
+        System.out.println();
+        System.out.println("2 - Display Customer");
+        System.out.println();
+        System.out.println("3 - Update Customer");
+        System.out.println();
+        System.out.println("4 - Delete Customer");
+        System.out.println();
+        System.out.println("5 - Back");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                addCustomer(customers, scanner);
+                break;
+            case 2:
+                displayCustomers(customers);
+                break;
+            case 3:
+                updateCustomer(customers, scanner);
+                break;
+            case 4:
+                System.out.println("DELETING CUSTOMERS");
+                break;
+            case 5:
+                displayMenu(scanner, customers, true);
+                break;
+            default:
+                System.out.println("Invalid Choice. ❌");
+        }
+    }
+    //Account Menu
+    //Transaction Menu
 }
